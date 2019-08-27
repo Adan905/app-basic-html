bb056c496d44f326516a2921fd1e5bccb50a6a15
alumno <alumno@E122>
2019-08-27 12:40:47 -0700

    pianojs

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const func =() =>{
     console.log(event.target.dataset.note);
 }
 
+const playnote = event =>{
+    const button = event.target;
+    const note = const note = button.dataset.note;
+    console.log(`audio${}`);
+    const audioId = `aidopS${note}`;
+    console.log(audioId);
+    const audio = document.getElementById(audioId);
+    audio.play();
+}
+
 buttons.forEach(
     button => button.addEventListener('click', playnote)
 );
